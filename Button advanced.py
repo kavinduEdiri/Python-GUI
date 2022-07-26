fc4257eb48e5dd2ac439ad68ec91c39a20deba64
Kavindu Prabhashana Edirisinghe <105447352+kavinduEdiri@users.noreply.github.com>
2022-07-26 21:23:46 +0530

    Add files via upload

---
+++ Button advanced.py
@@ -0,0 +1,37 @@
+from tkinter import *
+root=Tk()
+
+
+
+btn=Button(root,
+           text='button 1',
+           bg='yellow',
+           bd=20,
+           fg='Red',
+           activebackground="purple",
+           activeforeground="green",
+           width=10,
+           height=2
+
+
+
+
+
+
+
+           )
+btn.pack()
+
+
+
+
+
+
+
+
+
+
+root.maxsize(600,600)
+root.minsize(600,600)
+root.title("........Advanced button.....")
+root.mainloop()
